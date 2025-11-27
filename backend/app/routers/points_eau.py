119178f3bf0d389ac403798a771213f087274b7c
Valentin <hutacevaervalentin@gmail.com>
2025-11-27 16:18:55 +0100

    ajout option ajouter point full

--- backend/app/routers/points_eau.py
+++ backend/app/routers/points_eau.py
@@ -2,8 +2,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from api.database import get_db
-from api import crud
-from api.schemas import PointEauCreate
+import crud
+from schemas import PointEauCreate
 
 router = APIRouter()
 
