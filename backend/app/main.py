9289846e73684e16bae839598eabdf2542fd3816
tei.garnier <tei.garnier@gmail.com>
2025-11-28 15:12:22 +0100

    Ajout de commentaires, main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from .database import Base, engine
 from .routers.points_eau import router as points_router
 from .routers.utilisateurs import router as users_router
 from .routers.missions import router as missions_router
 from .routers.historique import router as historique_router
 
+#initialisation
 app = FastAPI(title="API FastAPI - FEN-Alim")
 
+#Autorise toutes les requêtes
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,8 +18,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#Crée les tables définies dans models.py si elles n'existent pas
 Base.metadata.create_all(bind=engine)
 
+#Routes
 app.include_router(points_router)
 app.include_router(users_router)
 app.include_router(missions_router)
@@ -27,4 +30,5 @@ app.include_router(historique_router)
 
 @app.get("/health")
 def health():
+    """Permet de vérifier que l'API fonctonne."""
     return {"status": "ok"}
